ffe726df12ba7ba62fbad41de7bd2ca1d4ef8895
saMu <samuelbarrantes1@gmail.com>
2025-05-20 10:36:35 -0600

    Add-apiAxios

---
+++ src/api/apiAxios.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosInstance } from "axios";
+
+
+const apiAxios: AxiosInstance = axios.create({
+  baseURL: 'https://localhost:7101/api',
+  timeout: 1000
+});
+
+
+apiAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default apiAxios;
