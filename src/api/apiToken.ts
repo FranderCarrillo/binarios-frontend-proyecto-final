202e360d74c07a4e1dd73bd32cf6b61fb548a98d
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-05-20 10:35:37 -0600

    add-apiToken

---
+++ src/api/apiToken.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiAxios from './apiAxios'; // la instancia configurada
+import type { Auth } from '../models/Auth/Auth';
+
+export async function login(auth: Auth): Promise<boolean> {
+  try {
+    const response = await axios.post<string>('https://localhost:7101/api/Login', auth);
+
+    const token = response.data;
+    if (token) {
+      localStorage.setItem('token', token);
+      apiAxios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      return true;
+    }
+
+    return false;
+  } catch (error) {
+    console.error("Error en autenticación:", error);
+    return false;
+  }
+}
