cae31644abe793ad22468f3a48ca26f967448d27
saMu <samuelbarrantes1@gmail.com>
2025-05-24 15:39:16 -0600

    cambios-varios

--- src/services/Candidate/CandidateService.ts
+++ src/services/Candidate/CandidateService.ts
@@ -2,6 +2,6 @@ import apiAxios from "../../api/apiAxios";
 import type { Candidate } from "../../models/Candidate/Candidate";
 
 export async function createCandidate(candidate: Candidate): Promise<Candidate>{
-    const response = await apiAxios.post(`/api/Candidates`, candidate);
+    const response = await apiAxios.post(`/Candidates`, candidate);
     return response.data;
 }
