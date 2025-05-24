23ecc15e0957672328893f10e0afb61c25f6d954
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-05-24 14:00:31 -0600

    fix-signUp

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,14 +1,14 @@
 // /src/router/auth.tsx
 import { createRoute, Outlet } from '@tanstack/react-router';
 import { rootRoute } from '../routes/__root';
+import SignUp from '../componets/SigIn/SignUp';
 
 const logoutLayoutRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/logout',
   component: () => (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h2 className="text-xl mb-4">Logout</h2>
-      
+      <SignUp />
       <Outlet />
     </div>
   ),
