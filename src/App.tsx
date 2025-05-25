76bb1bfad5b27dec48a5c155621ecb51abce9641
JOSE ROMAN FERNANDEZ <jose.roman.fernandez@est.una.ac.cr>
2025-05-25 12:07:09 -0600

    rename

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { rootRoute } from './routes/__root';
 import { privateRoutes } from './routes/private';
-import { authRoutes } from './components/Layouts/auth';
+import { authRoutes } from './components/Layouts/AuthLayout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { guestRoute } from './components/Layouts/SignUpLayout';
 
